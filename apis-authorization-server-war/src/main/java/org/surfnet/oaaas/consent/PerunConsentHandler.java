2c4b84aa071b20fd745ca9542882401cc3a3b55c
ondrejvelisek <ondrejvelisek@gmail.com>
2016-03-24 23:42:34 +0100

    Debug PerunAuthenticator

--- apis-authorization-server-war/src/main/java/org/surfnet/oaaas/consent/PerunConsentHandler.java
+++ apis-authorization-server-war/src/main/java/org/surfnet/oaaas/consent/PerunConsentHandler.java
@@ -7,11 +7,11 @@ public class PerunConsentHandler extends FormUserConsentHandler {
 
   @Override
   protected String getUserConsentUrl() {
-    return super.getUserConsentUrl();
+    return "/WEB-INF/jsp/perunConsent.jsp";
   }
 
   @Override
   protected String getUserConsentDeniedUrl() {
-    return super.getUserConsentDeniedUrl();
+    return "/WEB-INF/jsp/perunConsent_denied.jsp";
   }
 }
